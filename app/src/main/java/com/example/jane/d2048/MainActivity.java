ab6db2ce47328ed4cb0568431a4d97204ad909e8
Color <1185093725@qq.com>
2017-06-11 21:37:37 +0800

    Game2048withShare

---
+++ app/src/main/java/com/example/jane/d2048/MainActivity.java
@@ -0,0 +1,182 @@
+package com.example.jane.d2048;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.preference.PreferenceManager;
+import android.os.Bundle;
+import android.support.v4.media.RatingCompat;
+import android.view.KeyEvent;
+import android.view.Window;
+import com.example.jane.d2048.MainGame;
+import com.example.jane.d2048.wxapi.WXEntryActivity;
+
+public class MainActivity extends Activity {
+
+    MainView view;
+    public static final String WIDTH = "width";
+    public static final String HEIGHT = "height";
+    public static final String SCORE = "score";
+    public static final String HIGH_SCORE = "high score temp";
+    public static final String UNDO_SCORE = "undo score";
+    public static final String CAN_UNDO = "can undo";
+    public static final String UNDO_GRID = "undo";
+    public static final String GAME_STATE = "game state";
+    public static final String UNDO_GAME_STATE = "undo game state";
+    private Context context;
+    public long scoreshare ;
+
+    public static MainActivity instance = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//设置为无标题
+        view = new MainView(getBaseContext());//主界面
+        context = this;
+        instance= this;
+        SharedPreferences settings = PreferenceManager
+                .getDefaultSharedPreferences(this);
+        view.hasSaveState = settings.getBoolean("save_state", false);
+
+        //Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher);
+
+        if (savedInstanceState != null) {
+            if (savedInstanceState.getBoolean("hasState")) {
+                load();
+            }
+        }
+        setContentView(view);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_MENU) {
+            // Do nothing
+            return true;
+        } else if (keyCode == KeyEvent.KEYCODE_DPAD_DOWN) {
+            view.game.move(2);
+            return true;
+        } else if (keyCode == KeyEvent.KEYCODE_DPAD_UP) {
+            view.game.move(0);
+            return true;
+        } else if (keyCode == KeyEvent.KEYCODE_DPAD_LEFT) {
+            view.game.move(3);
+            return true;
+        } else if (keyCode == KeyEvent.KEYCODE_DPAD_RIGHT) {
+            view.game.move(1);
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putBoolean("hasState", true);
+        save();
+    }
+
+    protected void onPause() {
+        super.onPause();
+        save();
+    }
+
+    private void save() {
+        SharedPreferences settings = PreferenceManager
+                .getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = settings.edit();
+        Tile[][] field = view.game.grid.field;
+        Tile[][] undoField = view.game.grid.undoField;
+        editor.putInt(WIDTH, field.length);
+        editor.putInt(HEIGHT, field.length);
+        for (int xx = 0; xx < field.length; xx++) {
+            for (int yy = 0; yy < field[0].length; yy++) {
+                if (field[xx][yy] != null) {
+                    editor.putInt(xx + " " + yy, field[xx][yy].getValue());
+                } else {
+                    editor.putInt(xx + " " + yy, 0);
+                }
+
+                if (undoField[xx][yy] != null) {
+                    editor.putInt(UNDO_GRID + xx + " " + yy,
+                            undoField[xx][yy].getValue());
+                } else {
+                    editor.putInt(UNDO_GRID + xx + " " + yy, 0);
+                }
+            }
+        }
+        editor.putLong(SCORE, view.game.score);
+        editor.putLong(HIGH_SCORE, view.game.highScore);
+        editor.putLong(UNDO_SCORE, view.game.lastScore);
+        editor.putBoolean(CAN_UNDO, view.game.canUndo);
+        editor.putInt(GAME_STATE, view.game.gameState);
+        editor.putInt(UNDO_GAME_STATE, view.game.lastGameState);
+        editor.commit();
+    }
+
+    protected void onResume() {
+        super.onResume();
+        load();
+    }
+
+    private void load() {
+        // Stopping all animations
+        view.game.aGrid.cancelAnimations();
+
+        SharedPreferences settings = PreferenceManager
+                .getDefaultSharedPreferences(this);
+        for (int xx = 0; xx < view.game.grid.field.length; xx++) {
+            for (int yy = 0; yy < view.game.grid.field[0].length; yy++) {
+                int value = settings.getInt(xx + " " + yy, -1);
+                if (value > 0) {
+                    view.game.grid.field[xx][yy] = new Tile(xx, yy, value);
+                } else if (value == 0) {
+                    view.game.grid.field[xx][yy] = null;
+                }
+
+                int undoValue = settings.getInt(UNDO_GRID + xx + " " + yy, -1);
+                if (undoValue > 0) {
+                    view.game.grid.undoField[xx][yy] = new Tile(xx, yy,
+                            undoValue);
+                } else if (value == 0) {
+                    view.game.grid.undoField[xx][yy] = null;
+                }
+            }
+        }
+
+        view.game.score = settings.getLong(SCORE, view.game.score);
+        view.game.highScore = settings.getLong(HIGH_SCORE, view.game.highScore);
+        view.game.lastScore = settings.getLong(UNDO_SCORE, view.game.lastScore);
+        view.game.canUndo = settings.getBoolean(CAN_UNDO, view.game.canUndo);
+        view.game.gameState = settings.getInt(GAME_STATE, view.game.gameState);
+        view.game.lastGameState = settings.getInt(UNDO_GAME_STATE,
+                view.game.lastGameState);
+    }
+    public void shareqq(){
+        Intent intent = new Intent(MainActivity.this, WXEntryActivity.class);
+        startActivity(intent);
+    }
+
+    public void sharewx(){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "分享");
+        intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_text));
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(Intent.createChooser(intent, getTitle()));
+
+
+    }
+    public void sharesr(){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "分享");
+        intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_text) +
+                String.valueOf(view.game.score)+ "!  " + getString(R.string.share_address));
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(Intent.createChooser(intent, getTitle()));
+    }
+}
+
---
+++ app/src/main/java/com/example/jane/d2048/wxapi/WXEntryActivity.java
@@ -0,0 +1,15 @@
+package com.example.jane.d2048.wxapi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.jane.d2048.R;
+
+public class WXEntryActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wxentry);
+    }
+}
